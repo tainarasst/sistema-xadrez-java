c3a972474ba1cdb1541ef57718e2845c0da94e76
Tainara Silva Santos <tainara.silva.santos@hotmail.com>
2023-12-05 19:26:01 -0300

    Primeira Classe: Posição.

--- src/Aplicação/Programa.java
+++ src/Aplicação/Programa.java
@@ -1,10 +1,13 @@
 package Aplicação;
 
+import Tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 
-		System.out.println("Olá, mundo!");
+	Posicao pos = new Posicao(3, 5);
+	System.out.println(pos);
 		
 		
 	}
